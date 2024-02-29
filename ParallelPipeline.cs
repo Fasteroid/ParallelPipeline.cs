4c0cfc3879e3c23f9f8836b3c13fdad55ef97a60
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2024-02-29 16:57:44 -0500

    Initial commit

---
+++ ParallelPipeline.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Common {
+
+     /*
+     * ParallelPipeline by [not doxxing myself] a.k.a "Fasteroid"
+     * Written on 2/29/2024
+     * 
+     * Allows you to chain together a series of parallel stages.
+    */
+
+    public class ParallelPipeline<In, Out> {
+        
+        private IEnumerable<Task<Out>> Tasks { set; get; }
+
+        private ParallelPipeline(IEnumerable<Task<Out>> Tasks) {
+            this.Tasks = Tasks;
+        }
+
+        /// <summary>
+        /// Runs <paramref name="process"/> on each <paramref name="input"/> member in parallel.
+        /// </summary>
+        /// <param name="process">Do this to each element of <paramref name="input"/></param>
+        public static ParallelPipeline<In, Out> StartBy(IEnumerable<In> input, Func<In, Out> process) {
+            return new ParallelPipeline<In, Out>(
+                input.Select( element => Task.Run(() => process(element)) )
+            );
+        }
+
+        /// <summary>
+        /// Waits for all elements to process, then runs <paramref name="process"/> for each result in parallel.
+        /// </summary>
+        /// <param name="process">Do this for each result of the previous step</param>
+        public ParallelPipeline<Out, NextOut> ThenAll<NextOut>( Func<Out, NextOut> process ) {
+            return new ParallelPipeline<Out, NextOut>(
+                Task.WhenAll(Tasks).ContinueWith( 
+                    all => all.Result.Select( element => Task.Run(() => process(element)) ) 
+                ).Result
+            );
+        }
+
+        /// <summary>
+        /// Call at the end of the pipeline and await it to eventually get the results.
+        /// </summary>
+        public async Task<List<Out>> Results() {
+            return (await Task.WhenAll(Tasks)).ToList();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Common;
+using System;
+
+
+List<int> victims = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+var results = await ParallelPipeline<int, int>.StartBy(victims, (int victim) => { 
+        RandomWaiter.Wait();
+        Console.WriteLine($"{victim} did step 0...");
+        return victim;
+    }).ThenAll( (int victim) => {
+        RandomWaiter.Wait();
+        Console.WriteLine($"{victim} did step 1...");
+        return victim;
+    }).ThenAll( (int victim) => {
+        RandomWaiter.Wait();
+        Console.WriteLine($"{victim} did step 2...");
+        return victim;
+    }).Results();
+
+Console.WriteLine(string.Join(", ", results)); // 9, 15, 21, 27, 33, 39, 45, 51, 57, 63
+
+
+public static class RandomWaiter {
+    public static void Wait() {
+        Random random = new Random();
+        Thread.Sleep(random.Next(1000, 2000));
+    }
+}
